00d0e7b33fe688193d7dcf1e917b9d8f7c9a8a31
Sasha <korostyshevskiy@gmail.com>
2018-03-25 01:02:43 +0200

    Problem S solved

--- Korostyshevskyi/Problems/main.cpp
+++ Korostyshevskyi/Problems/main.cpp
@@ -7,10 +7,23 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    double a;
 
-    cin >> a;
+int n = 0;
+cin >> n;
+double a[200000];
+for (int i = 0; i < n; i++){
+  cin >> a[i];
+}
+  int ans = a[0],
+	sum = 0,
+	min_sum = 0;
+for (int r=0; r<n; ++r) {
+	sum += a[r];
+	ans = max (ans, sum - min_sum);
+	min_sum = min (min_sum, sum);
 
-    cout << a;
+}
+
+    cout << ans;
     return 0;
 }
